68b8e513f5a87eaa3fbbf30e64621dbd9d30d511
Luke Mulholland <lukemulholland@gmail.com>
2025-05-25 06:22:08 +1000

    Add initial activities data structure and script file

---
+++ activities.py
@@ -0,0 +1,5 @@
+activities = [
+    {"name": "Go for a hike", "type": "outdoor", "time": 180, "weather": ["sunny", "cloudy"]},
+    {"name": "Visit a museum", "type": "indoor", "time": 120, "weather": ["sunny", "rainy"]},
+    ...
+]
